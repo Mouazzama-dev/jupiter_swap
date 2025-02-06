8d23fc72d0ec4823d916e101d38bf72774103912
Mouazzama-dev <mouazzamabinteumer.ep19102044@gmail.com>
2025-02-06 04:18:45 +0100

    Setting up Jupiter CPI using its program Id

--- programs/jupiter_swap/src/lib.rs
+++ programs/jupiter_swap/src/lib.rs
@@ -1,4 +1,37 @@
 use anchor_lang::prelude::*;
+use anchor_lang::{
+    prelude::*,
+    solana_program::{entrypoint::ProgramResult, instruction::Instruction, program::invoke_signed},
+    system_program,
+};
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
+
+pub const AUTHORITY_SEED: &[u8] = b"authority";
+pub const WSOL_SEED: &[u8] = b"wsol";
+
+mod jupiter {
+    use anchor_lang::declare_id;
+    declare_id!("JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4"); // Jupiter Aggregator ID
+}
+
+#[derive(Clone)]
+pub struct Jupiter;
+
+impl anchor_lang::Id for Jupiter {
+    fn id() -> Pubkey {
+        jupiter::id()
+    }
+}
+
+#[error_code]
+pub enum ErrorCode {
+    InvalidReturnData,
+    InvalidJupiterProgram,
+    IncorrectOwner,
+    SwapFailed,
+}
+
+
 
 declare_id!("71gmhF6PWXzKV1xhkv4DmQikAhCL5zXSQGccLwurzZd5");
 
@@ -10,7 +43,11 @@ pub mod jupiter_swap {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+
