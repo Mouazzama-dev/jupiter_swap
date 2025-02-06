56d64d3e2d430559b3ce438a7526baebc9760bd6
Mouazzama-dev <mouazzamabinteumer.ep19102044@gmail.com>
2025-02-06 04:29:36 +0100

    Sol conversion to wSOL

--- programs/jupiter_swap/src/lib.rs
+++ programs/jupiter_swap/src/lib.rs
@@ -39,12 +39,30 @@ declare_id!("71gmhF6PWXzKV1xhkv4DmQikAhCL5zXSQGccLwurzZd5");
 pub mod jupiter_swap {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn swap_sol_to_memes(ctx: Context<SolToMemeSwap>, data: Vec<u8>) -> Result<()> {
+        let authority_bump = ctx.bumps.get("program_authority").unwrap().to_le_bytes();
+        let wsol_bump = ctx.bumps.get("program_wsol_account").unwrap().to_le_bytes();
+
+        let total_sol = ctx.accounts.user_account.to_account_info().lamports();
+        require!(total_sol > 0, ErrorCode::SwapFailed);
+
+        // Split SOL into 3 equal parts
+        let sol_split = total_sol / 3; 
+
+        //Jupiter Aggregator only supports token swaps when the SOL is wrapped into wSOL (wrapped SOL) to participate in these swaps.
+        msg!("Creating wSOL account...");
+        create_wsol_token_idempotent(
+            ctx.accounts.program_authority.clone(),
+            ctx.accounts.program_wsol_account.clone(),
+            ctx.accounts.sol_mint.clone(),
+            ctx.accounts.token_program.clone(),
+            ctx.accounts.system_program.clone(),
+            &authority_bump,
+            &wsol_bump,
+        )?;
+
         Ok(())
     }
-
-    
 }
 
 #[derive(Accounts)]
